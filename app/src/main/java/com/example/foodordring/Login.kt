0970e2b0e98bac5c1f42ea89a9147fedf913ee61
Shashi8969 <shashikumar10062@gmail.com>
2024-08-28 01:11:39 +0530

    Successfully create login screen

--- app/src/main/java/com/example/foodordring/Login.kt
+++ app/src/main/java/com/example/foodordring/Login.kt
@@ -1,29 +1,20 @@
 package com.example.foodordring
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class StartActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_start)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btn_next = findViewById(R.id.btn_next) as Button
-// set on-click listener
-        btn_next.setOnClickListener {
-            val i = Intent(this, Login::class.java);
-            startActivity(i);
         }
     }
 }
