1e69f1daef5047a1fbd7cdccf324a27a10298283
Shashi8969 <shashikumar10062@gmail.com>
2024-08-30 15:53:55 +0530

    create fragments

--- app/src/main/java/com/example/foodordring/MainActivity.kt
+++ app/src/main/java/com/example/foodordring/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.foodordring
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        var NavController : NavController = findNavController(R.id.fragmentContainerView)
+        var bottomnav : BottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomnav.setupWithNavController(NavController)
     }
 }
