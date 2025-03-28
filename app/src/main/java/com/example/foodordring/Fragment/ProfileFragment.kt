58ee75ee41882d732a06273795e4840ee43f2785
Shashi8969 <shashikumar10062@gmail.com>
2025-03-28 22:51:21 +0530

    Successfull connect with firebase

--- app/src/main/java/com/example/foodordring/Fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/foodordring/Fragment/ProfileFragment.kt
@@ -7,17 +7,27 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.foodordring.R
 import com.example.foodordring.databinding.FragmentProfileBinding
+import com.example.foodordring.model.UserModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class ProfileFragment : Fragment() {
 
+    //Create Firebase auth
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
     private lateinit var binding: FragmentProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
@@ -25,8 +35,34 @@ class ProfileFragment : Fragment() {
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        setUserData()
     }
 
+    private fun setUserData() {
+        val userId = auth.currentUser?.uid
+        if (userId != null) {
+            val userReference = database.reference.child("users").child(userId)
 
+            userReference.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
 
+                        val userProfile = snapshot.getValue(UserModel::class.java)
+                        if (userProfile != null) {
+                            binding.profileName.setText(userProfile.name)
+                            binding.profileAddress.setText(userProfile.address)
+                            binding.profileEmail.setText(userProfile.email)
+                            binding.profilePhone.setText(userProfile.phone)
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
+
+        }
+    }
 }
